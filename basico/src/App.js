392fd240eab085339de45ff095bfc82c5f0e50e3
Pedro Arns Junior <parnsjr@gmail.com>
2022-04-13 17:17:40 -0300

    AppControllerText

--- basico/src/App.js
+++ basico/src/App.js
@@ -1,20 +1,20 @@
 import './App.css';
 import React from 'react';
 
-
 //import AppCondOperadorLogico from './AppCondOperadorLogico';
 //import AppCondPrevComponente from './AppCondPrevComponente';
 //import AppCondElemVariavel from './AppCondElemVariavel';
 //import AppListaDeNumerosV2 from './AppListaDeNumerosV2';
 //import AppUsoCorretoDeChaves from './AppUsoCorretoDeChaves';
 //import AppListaChavesUnicas from './AppListaChavesUnicas';
-import AppAdicionarMap from './AppAdicionarMap';
+//import AppAdicionarMap from './AppAdicionarMap';
+import AppControllerText from './AppControllerText';
 
 function App() {
   return (
     <div className='App'>
       
-      <AppAdicionarMap />      
+      <AppControllerText />      
     </div>
   );
 }
